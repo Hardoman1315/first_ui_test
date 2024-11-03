34120613c19dc1de695e69871fc016d60f9aee7b
Hardoman1315 <hardomangithub@gmail.com>
2024-11-03 16:22:28 +0400

    Added missing locators, corrected some typos

--- test_run.py
+++ test_run.py
@@ -1,3 +1,5 @@
+import time
+
 from pages import BasePage, LoginPage, InventoryPage, ItemPage, CartPage
 
 
@@ -5,15 +7,25 @@ def test_est_1_login(driver):
     auth_page = LoginPage(driver)
     auth_page.auth('standard_user', 'secret_sauce')
 
+    time.sleep(2)
+
     inventory_page = InventoryPage(driver)
     inventory_page.choose_item()
 
+    time.sleep(2)
+
     item_page = ItemPage(driver)
     item_page.add_to_cart_btn_click()
-    item_page.back_to_products()
+    item_page.return_to_products()
+
+    time.sleep(2)
 
     inventory_page.add_jacket_to_cart_btn_click()
     inventory_page.cart_btn_click()
 
+    time.sleep(2)
+
     cart_page = CartPage(driver)
     assert cart_page.number_of_products() == 2
+
+    time.sleep(5)
